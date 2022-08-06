8a8d6198760bebe13e98246b4be89d17f38ed966
jobdev-hub <job.methods@outlook.com>
2022-08-06 15:47:20 -0300

    created initial struct

---
+++ src/main/java/br/com/jobdev/msdigitalgym/MsDigitalGymApplication.java
@@ -0,0 +1,16 @@
+package br.com.jobdev.msdigitalgym;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API - Academia Digital", version = "1.0.0"))
+public class MsDigitalGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsDigitalGymApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/jobdev/msdigitalgym/MsDigitalGymApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jobdev.msdigitalgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDigitalGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
